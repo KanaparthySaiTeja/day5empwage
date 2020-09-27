6107f04ff01bb8483b21b7905ea0631678896914
saiteja-code <saitejakanaparthy@gmail.com>
2020-09-27 20:18:02 +0530

    uc1 added

--- src/main/java/empwage/wage/employwage.java
+++ src/main/java/empwage/wage/employwage.java
@@ -9,6 +9,14 @@ public class employwage
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome to Employee Wage Computation" );
-    }
-}
+     	  {
+  	    	int IS_FULL_TIME=1;
+  	        double empCheck = Math.floor(Math.random() * 10)%2;
+  			
+  	    			
+  	    	if(empCheck == IS_FULL_TIME) {
+  	    	System.out.println("Employee present");
+  	    	} else {
+  	    	System.out.println("Employee absent");
+  	    	}
+  	  }}}
