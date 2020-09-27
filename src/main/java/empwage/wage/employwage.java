53376520ca662b344eb3d06d624697c135a8e59b
saiteja-code <saitejakanaparthy@gmail.com>
2020-09-27 20:20:16 +0530

    uc2 added

--- src/main/java/empwage/wage/employwage.java
+++ src/main/java/empwage/wage/employwage.java
@@ -7,8 +7,18 @@ package empwage.wage;
 public class employwage
 
 {
-    public static void main( String[] args )
+	public static void main( String[] args )
     {
-        System.out.println( "Welcome to Employee Wage Computation" );
-    }
-}
+    	int IS_FULL_TIME=1;
+    	int EMP_RATE_HOUR=20;
+    	
+    	int empHrs=0;
+    	int empWage=0;
+    	
+    	double empCheck =Math.floor(Math.random() *10)%2;
+    	if(empCheck==IS_FULL_TIME)
+    		empHrs=8;
+    	else
+    		empHrs=0;
+    	empWage=empHrs*EMP_RATE_HOUR;
+    	System.out.println("Emp Wage: " +empWage);}}
