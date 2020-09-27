bc6c4780fdd6618cdb410ae6c75092a3c5a71669
saiteja-code <saitejakanaparthy@gmail.com>
2020-09-27 20:21:58 +0530

    uc3 added

--- src/main/java/empwage/wage/employwage.java
+++ src/main/java/empwage/wage/employwage.java
@@ -7,8 +7,22 @@ package empwage.wage;
 public class employwage
 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Welcome to Employee Wage Computation" );
-    }
-}
+	 public static void main( String[] args )
+	    {
+	    	
+	    	int IS_FULL_TIME=2;
+	    	int IS_PART_TIME=1;
+	    	int EMP_RATE_HOUR=20;
+	    	
+	    	int empHrs=0;
+	    	int empWage=0;
+	    	
+	    	double empCheck =Math.floor(Math.random() *10)%3;
+	    	if(empCheck==IS_FULL_TIME)
+	    		empHrs=8;
+	    	else if (empCheck==IS_PART_TIME)
+	    		empHrs=4;
+	    	else
+	    		empHrs=0;
+	    	empWage=empHrs*EMP_RATE_HOUR;
+	    	System.out.println("Emp Wage: " +empWage);}}
