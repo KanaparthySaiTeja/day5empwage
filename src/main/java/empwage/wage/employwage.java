4e66d857ca9d4be56beaa999d97de088ad12f2bf
saiteja-code <saitejakanaparthy@gmail.com>
2020-09-27 20:23:56 +0530

    uc4 added

--- src/main/java/empwage/wage/employwage.java
+++ src/main/java/empwage/wage/employwage.java
@@ -7,22 +7,29 @@ package empwage.wage;
 public class employwage
 
 {
-	 public static void main( String[] args )
-	    {
-	    	
-	    	int IS_FULL_TIME=2;
-	    	int IS_PART_TIME=1;
-	    	int EMP_RATE_HOUR=20;
-	    	
-	    	int empHrs=0;
-	    	int empWage=0;
-	    	
-	    	double empCheck =Math.floor(Math.random() *10)%3;
-	    	if(empCheck==IS_FULL_TIME)
-	    		empHrs=8;
-	    	else if (empCheck==IS_PART_TIME)
-	    		empHrs=4;
-	    	else
-	    		empHrs=0;
-	    	empWage=empHrs*EMP_RATE_HOUR;
-	    	System.out.println("Emp Wage: " +empWage);}}
+	public static final int IS_FULL_TIME=2;
+	public static final int IS_PART_TIME=1;
+	public static final int EMP_RATE_HOUR=20;
+	
+
+    public static void main( String[] args )
+    {
+    	
+    	
+    	int empHrs=0;
+    	int empWage=0;
+    	
+    	int empCheck =(int) Math.floor(Math.random() *10)%3;
+    	switch(empCheck) {
+        case IS_FULL_TIME:
+    		empHrs=8;
+    		break;
+        case IS_PART_TIME:
+    		empHrs=4;
+    		break;
+    	default :
+    		empHrs=0;
+    		break;
+    }
+    	empWage=empHrs*EMP_RATE_HOUR;
+    	System.out.println("Emp Wage: " +empWage);}}
